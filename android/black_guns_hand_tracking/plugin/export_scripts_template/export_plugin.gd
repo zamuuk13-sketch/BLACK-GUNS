0c4b21e85edc84b7a2e31adec077cbe7be265494
zamuuk13-sketch <zamuuk13@gmail.com>
2026-09-19 17:48:06 -0300

    Register Stage 5 AAR and MediaPipe dependency for Godot export

--- android/black_guns_hand_tracking/plugin/export_scripts_template/export_plugin.gd
+++ android/black_guns_hand_tracking/plugin/export_scripts_template/export_plugin.gd
@@ -1,7 +1,18 @@
 @tool
 extends EditorExportPlugin
+
 const _plugin_name := "BlackGunsHandTracking"
-func _get_name() -> String: return _plugin_name
-func _supports_platform(platform: EditorExportPlatform) -> bool: return platform is EditorExportPlatformAndroid
+
+func _get_name() -> String:
+    return _plugin_name
+
+func _supports_platform(platform: EditorExportPlatform) -> bool:
+    return platform is EditorExportPlatformAndroid
+
+func _get_android_libraries(platform: EditorExportPlatform, debug: bool) -> PackedStringArray:
+    if debug:
+        return PackedStringArray(["BlackGunsHandTracking/bin/debug/BlackGunsHandTracking-debug.aar"])
+    return PackedStringArray(["BlackGunsHandTracking/bin/release/BlackGunsHandTracking-release.aar"])
+
 func _get_android_dependencies(platform: EditorExportPlatform, debug: bool) -> PackedStringArray:
     return PackedStringArray(["com.google.mediapipe:tasks-vision:0.10.26"])
